e0a0a4e23fbbb3c01019ac4d9bcd720e23df0da5
YaJunCaiTW <33884426+YaJunCaiTW@users.noreply.github.com>
2021-08-08 00:06:18 +0800

    20210807刻板 選單 Canvas基本畫面

---
+++ LightWhisper/js/MyCavas.js
@@ -0,0 +1,13 @@
+var backgroundImg = document.createElement("IMG");
+var canvas = document.getElementById("MyCanvas");
+var ctx = canvas.getContext("2d");
+
+function InitialMyCanvas(){
+   // backgroundImg.setAttribute("src", "img/basicmode.jpg");
+    //console.log(backgroundImg.src);
+    /*backgroundImg.onload =
+    function (){
+        ctx.drawImage(backgroundImg, 0, 0);
+    };*/
+    //document.body.appendChild(backgroundImg);
+}
