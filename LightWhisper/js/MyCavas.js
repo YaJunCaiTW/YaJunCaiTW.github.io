7b2e7c780733ff51f32495247e7769b601e4d9bd
YaJunCaiTW <33884426+YaJunCaiTW@users.noreply.github.com>
2021-08-08 11:03:05 +0800

    20210808更改路徑

--- LightWhisper/js/MyCavas.js
+++ LightWhisper/js/MyCavas.js
@@ -1,13 +1,3 @@
-var backgroundImg = document.createElement("IMG");
 var canvas = document.getElementById("MyCanvas");
 var ctx = canvas.getContext("2d");
 
-function InitialMyCanvas(){
-   // backgroundImg.setAttribute("src", "img/basicmode.jpg");
-    //console.log(backgroundImg.src);
-    /*backgroundImg.onload =
-    function (){
-        ctx.drawImage(backgroundImg, 0, 0);
-    };*/
-    //document.body.appendChild(backgroundImg);
-}
